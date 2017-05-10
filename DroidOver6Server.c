eb009d8cbc0dec83057d50674d75e3e267d07b1c
Jason Lau <i@dotkrnl.com>
2017-05-10 11:20:04 +0800

    Updated DroidOver6 server.

---
+++ DroidOver6Server.c
@@ -0,0 +1,258 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <bits/ioctls.h>
+#include <errno.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+
+typedef struct epoll_event epoll_event;
+
+const static int IP_PACKET_MAX_SIZE = 65536;
+const static int PIPE_BUF_LEN = 65536 * 100;
+const static int SERVER_PORT = 13872;
+const static int BUFFER_LENGTH = 250;
+
+const static uint8_t TYPE_REQUEST = 102;
+const static uint8_t TYPE_REPLY = 103;
+const static uint8_t TYPE_HEART = 104;
+
+typedef struct {
+    uint32_t length;
+    uint8_t type;
+    uint8_t data[];
+} __attribute__((packed)) over6Packet;
+
+static int tunOpen(char *devname) {
+    struct ifreq ifr;
+    int fd, err;
+
+    if ((fd = open("/dev/net/tun", O_RDWR)) == -1) {
+        perror("open /dev/net/tun");
+        exit(1);
+    }
+    memset(&ifr, 0, sizeof(ifr));
+    ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+    strncpy(ifr.ifr_name, devname, IFNAMSIZ);
+
+    /* ioctl will use if_name as the name of TUN
+     * interface to open: "tun0", etc. */
+    if ((err = ioctl(fd, TUNSETIFF, (void *) &ifr)) == -1) {
+        perror("ioctl TUNSETIFF");
+        close(fd);
+        exit(1);
+    }
+
+    /* After the ioctl call the fd is "connected" to tun device specified
+     * by devname */
+    return fd;
+}
+
+static int readToBuf(int fd, uint8_t *buffer, int *used) {
+    int total = 0, ret = 0;
+    const size_t READ_SIZE = IP_PACKET_MAX_SIZE;
+    while ((*used) < PIPE_BUF_LEN - READ_SIZE) {
+        ret = read(fd, buffer + *used, READ_SIZE);
+        if (ret <= 0) break;
+        (*used) += ret;
+        total += ret;
+    }
+    printf(" >%d< ", total);
+    return ret;
+}
+
+static void over6Handle(int fd, uint8_t *buffer, int *used) {
+    for (;;) {  // for all packets
+        if ((*used) < 4) return;  // need more read
+
+        over6Packet *data = (over6Packet *) buffer;
+        uint32_t len = ntohl(data->length);
+        if (*used < len) return; // packet not complete
+
+        size_t actual_size = len - sizeof(over6Packet);
+
+        if (data->type == TYPE_HEART) {
+            // ignore
+        } else if (data->type == TYPE_REQUEST) {
+            // send packet to raw network
+            int ret = 0;
+            if ((ret = write(fd, data->data, actual_size)) < actual_size) {
+                printf("\nERROR ->RAW size = %d err = %d\n", len, ret);
+            }
+        } else {
+            printf("\nWARN Not implemented yet\n");
+        }
+
+        printf(" ]%d[ ", len);
+        memmove(buffer, buffer + len, *used - len);
+        *used -= len;
+    }
+}
+
+static void rawToOver6(int fd, uint8_t *buffer, int *used) {
+    if (!*used) return;  // no data
+    over6Packet header;
+    header.type = TYPE_REPLY;
+    header.length = htonl(*used + sizeof(header));
+    int temp;
+    if ((temp = write(fd, &header, sizeof(header))) < sizeof(header)) {
+        printf("ERROR ->O6 size = %d err = %d\n", sizeof(header), temp);
+    }
+    if ((temp = write(fd, buffer, *used)) < *used) {
+        printf("ERROR ->O6 size = %d err = %d\n", *used, temp);
+    }
+    printf(" [%d] ", *used + sizeof(header));
+    *used = 0;
+}
+
+static int createEpollFd() {
+    int fd = epoll_create1(0);
+    if (fd == -1) return -1;
+    return fd;
+}
+
+static int addToEpollFd(int epollFd, int *fds, int count) {
+    int i;
+    for (i = 0; i < count; i++) {
+        int flags = fcntl(fds[i], F_GETFL, 0);
+        if (flags == -1) return 0;
+        flags |= O_NONBLOCK;  // set fds[i] to be nonblocking
+        if (fcntl(fds[i], F_SETFL, flags)) return 0;
+        struct epoll_event event;
+        event.data.fd = fds[i];
+        event.events = EPOLLIN | EPOLLOUT | EPOLLERR | EPOLLHUP;
+        if (epoll_ctl(epollFd, EPOLL_CTL_ADD, fds[i], &event) == -1) return 0;
+    }
+    return 1;
+}
+
+int main(int argc, uint8_t *argv[]) {
+    int server_fd = -1, tun_dev_fd, over6_fd = -1;
+
+    // init tun dev
+    if (argc < 2) {
+        tun_dev_fd = tunOpen("over6");
+    } else {
+        tun_dev_fd = tunOpen(argv[1]);
+    }
+
+    // init server socket
+    int rc, on = 1, rcdsize = BUFFER_LENGTH;
+    uint8_t buffer[BUFFER_LENGTH];
+    struct sockaddr_in6 serveraddr, clientaddr;
+    int addrlen = sizeof(clientaddr);
+    uint8_t str[INET6_ADDRSTRLEN];
+    if ((server_fd = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
+        perror("\nERROR socket() failed");
+        goto failed;
+    }
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+                   (uint8_t *) &on, sizeof(on)) < 0) {
+        perror("\nERROR setsockopt(SO_REUSEADDR) failed");
+        goto failed;
+    }
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin6_family = AF_INET6;
+    serveraddr.sin6_port = htons(SERVER_PORT);
+    serveraddr.sin6_addr = in6addr_any;
+
+    // listen on server
+    if (bind(server_fd, (struct sockaddr *) &serveraddr,
+             sizeof(serveraddr)) < 0) {
+        perror("\nERROR bind() failed");
+        goto failed;
+    }
+    if (listen(server_fd, 10) < 0) {
+        perror("\nERROR listen() failed");
+        goto failed;
+    }
+
+    printf("\nINFO Ready for client connect().\n");
+
+    if ((over6_fd = accept(server_fd, NULL, NULL)) < 0) {
+        perror("\nERROR accept() failed");
+        goto failed;
+    } else {
+        getpeername(over6_fd, (struct sockaddr *) &clientaddr, &addrlen);
+        if (inet_ntop(AF_INET6, &clientaddr.sin6_addr, str, sizeof(str))) {
+            printf("\nINFO Client address: %s\n", str);
+            printf("INFO Client port: %d\n", ntohs(clientaddr.sin6_port));
+        }
+    }
+
+    uint8_t *ipBuffer = malloc(PIPE_BUF_LEN);
+    int ipBufferUsed = 0;
+    uint8_t *over6PacketBuffer = malloc(PIPE_BUF_LEN);
+    int over6PacketBufferUsed = 0;
+
+    int epollFd = createEpollFd();
+    if (epollFd < 0) {
+        printf("\nCannot initialize I/O multiplex.\n");
+        goto failed;
+    }
+    int fds[2] = {over6_fd, tun_dev_fd};
+    if (!addToEpollFd(epollFd, fds, 2)) {
+        printf("\nCannot initialize I/O multiplex.\n");
+        goto failed;
+    }
+
+    epoll_event *events = (epoll_event *) calloc(10, sizeof(epoll_event));
+    // Event loop
+    for (;;) {
+        int eventCount = epoll_wait(epollFd, events, 10, -1);
+        int i, handled_read = 0;
+        for (i = 0; i < eventCount; i++) {
+            // TODO failed to detect client disconnect
+            if ((events[i].events & EPOLLERR) || (events[i].events & EPOLLHUP)) {
+                printf("\nERROR epoll event: err or hup\n");
+                if (events[i].data.fd == over6_fd) printf("ERROR on over6\n");
+                else printf("ERROR on tun\n");
+                goto failed;
+            } else if (!(events[i].events & (EPOLLIN | EPOLLOUT))) {
+                printf("\nWARN Unknown event\n");
+                continue;
+            }
+
+            if (events[i].events & EPOLLIN) {
+                if (events[i].data.fd == tun_dev_fd) {
+                    readToBuf(tun_dev_fd, ipBuffer, &ipBufferUsed);
+                    handled_read += 1;
+                } else if (events[i].data.fd == over6_fd) {
+                    readToBuf(over6_fd, over6PacketBuffer, &over6PacketBufferUsed);
+                    handled_read += 1;
+                }
+            }
+
+            if (events[i].events & EPOLLOUT) {
+                if (events[i].data.fd == tun_dev_fd) {
+                    over6Handle(tun_dev_fd, over6PacketBuffer, &over6PacketBufferUsed);
+                } else if (events[i].data.fd == over6_fd) {
+                    rawToOver6(over6_fd, ipBuffer, &ipBufferUsed);
+                }
+            }
+        }
+        if (handled_read) printf("\nHandled read: %d\n", handled_read);
+    }
+    return 0;
+
+    failed:
+    /***********************************************************************/
+    /* Close down any open socket descriptors                              */
+    /***********************************************************************/
+    if (server_fd != -1) close(server_fd);
+    if (over6_fd != -1) close(over6_fd);
+    if (tun_dev_fd != -1) close(tun_dev_fd);
+    return EXIT_FAILURE;
+}
